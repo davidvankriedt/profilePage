d3eaa7149e282428eb471dee70f2aad1537e4386
davidvankriedt <davidvankriedt@icloud.com>
2024-09-03 21:17:19 +1000

    JS, html, and css changes

---
+++ index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.showButton');
+const container = document.querySelector('.linkContainer');
+
+
+function show() {
+    button.style.display = "none";
+    container.style.display = "flex";
+}
